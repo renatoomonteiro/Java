c89b91855857c884b65a9a8fa2c33ef0244115bc
Renato Monteiro <reoliveiramonteiro.rm@gmail.com>
2022-06-23 08:17:34 -0300

    Adicionei faculdade e clinica

---
+++ clinica/src/tela/TelaPrincipal.java
@@ -0,0 +1,8 @@
+package tela;
+
+public class TelaPrincipal {
+	//Tudo começa a partir de uma MAIN!
+	public static void main(String[] args) {
+
+	}
+}
---
+++ faculdade/src/administracao/Agenda.java
@@ -0,0 +1,7 @@
+package administracao;
+/*
+ * Exercíco de criação de projeto, pacotes e classes
+ * */
+public class Agenda {
+
+}
