a5fddab02c6cfef40f92b2cae4a875e73c8b4722
Renato Monteiro <reoliveiramonteiro.rm@gmail.com>
2022-06-25 10:00:07 -0300

    Criei o pacote Lambda

---
+++ exercicios/src/lambdas/Calculo.java
@@ -0,0 +1,7 @@
+//Aula 172 - Primeira Função Lambda
+
+package lambdas;
+
+public interface Calculo {
+	public abstract double executar(double a, double b); //A classe Calculo possui um método executar, que recebe dois parâmetros.
+}
