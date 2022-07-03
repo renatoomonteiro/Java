7ec5155d343badba3a7c70df65b7a613d09317b8
Renato Monteiro <reoliveiramonteiro.rm@gmail.com>
2022-07-02 23:06:23 -0300

    Criei as classes Media, Reduce1, 2 e 3

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -3,10 +3,16 @@ package streams;
 public class Aluno {
 	final String nome;
 	final Double nota;
+	final boolean bomComportamento;
 
 	public Aluno(String nome, Double nota) {
+		this(nome, nota, true); // Chamada do método construtor abaixo, sem precisar inserir mais um parâmetro
+	}
+
+	public Aluno(String nome, Double nota, boolean bomComportamento) {
 		this.nome = nome;
 		this.nota = nota;
+		this.bomComportamento = bomComportamento;
 	}
 
 }
--- exercicios/src/streams/Filter.java
+++ exercicios/src/streams/Filter.java
@@ -9,13 +9,13 @@ public class Filter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Aluno a1 = new Aluno("Ana", 6.8);
-		Aluno a2 = new Aluno("Bia", 5.8);
-		Aluno a3 = new Aluno("Daniel", 3.8);
-		Aluno a4 = new Aluno("Gui", 6.8);
-		Aluno a5 = new Aluno("Regiane", 7.1);
-		Aluno a6 = new Aluno("Renato", 8.8);
-		Aluno a7 = new Aluno("Gabriel", 10.8);
+		Aluno a1 = new Aluno("Ana", 6.8, true);
+		Aluno a2 = new Aluno("Bia", 5.8, true);
+		Aluno a3 = new Aluno("Daniel", 3.8, true);
+		Aluno a4 = new Aluno("Gui", 6.8, true);
+		Aluno a5 = new Aluno("Regiane", 7.1, true);
+		Aluno a6 = new Aluno("Renato", 8.8, true);
+		Aluno a7 = new Aluno("Gabriel", 10.8, true);
 		
 		
 		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6, a7);
