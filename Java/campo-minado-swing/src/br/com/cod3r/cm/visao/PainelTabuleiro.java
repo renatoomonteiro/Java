f2e14e129017007a65b50f39bfb8966a88241e24
renatoomonteiro <reoliveiramonteiro.rm@gmail.com>
2022-07-23 22:33:00 -0300

    Adicionei as classes PainelTabuleiro e TelaPrincipal

---
+++ Java/campo-minado-swing/src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,17 @@
+//Aula 260 - Interface Gráfica do Campo Minado #01
+
+package br.com.cod3r.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+//JPanel = é um container que agrupa vários tipos de componentes
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel{
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+	}
+
+}
---
+++ Java/campo-minado-swing/src/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -0,0 +1,28 @@
+//Aula 260 - Interface Gráfica do Campo Minado #01
+
+package br.com.cod3r.cm.visao;
+
+import javax.swing.JFrame;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame {
+	// Propriedades da janela principal
+	public TelaPrincipal() { // Linhas, Colunas e Minas
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		add(new PainelTabuleiro(tabuleiro));
+
+		setTitle("Campo Minado");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		// setDefaultCloseOperation(DISPOSE_ON_CLOSE) = Encerra o processo através do
+		// botão fechar da janela;
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new TelaPrincipal();
+	}
+}
