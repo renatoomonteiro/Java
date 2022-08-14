27939b0b2dc62f4def75549725c2bd3654eb969c
Renato Monteiro <reoliveiramonteiro.rm@gmail.com>
2022-08-13 21:28:12 -0300

    Criei o projeto exercicios-jpa
    Adição via upload em um projeto que utiliza o padrão JPA (Java Persistence API), para facilitar a integração da aplicação com o banco de dados.

---
+++ exercicios-jpa/src/main/java/modelo/basico/Usuario.java
@@ -0,0 +1,48 @@
+//Aula 330 - Entidade Usuário
+
+package modelo.basico;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+// A classe e a tabela devem ter o mesmo nome
+@Entity
+public class Usuario {
+	@Id
+	private Long id;
+	private String nome, email;
+
+	public Usuario() {
+
+	}
+
+	public Usuario(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
